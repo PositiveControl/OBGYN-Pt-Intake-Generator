b7776c41fb9504b61d42bca564ff5ef738f131e6
Mark Evans <ga1j1n@mac.com>
2014-04-02 10:01:46 -0600

    User sees output in textarea on patients page
    added textarea to patients page
    replaced html line breaks with &#13;
    changed index h1 and footer messages

--- spec/pt_spec.rb
+++ spec/pt_spec.rb
@@ -6,7 +6,7 @@ Capybara.app = App
 feature 'App generates text from dropdown and form inputs' do
   scenario 'User sees welcome on index' do
     visit ('/')
-    expect(page).to have_content "Welcome to tPIG - Tiare's Pt. Intake Generator"
+    expect(page).to have_content "Welcome to tPIG - T's Pt. Intake Generator"
     fill_in "patient[age]", :with => "22"
     click_on "Generate!"
     expect(page).to have_content "22"
