8b606a8a21d4cd1ffc2760af139caf7efbb40513
Mark Evans <ga1j1n@mac.com>
2014-04-04 21:39:54 -0600

    add credentials for text to send.

--- lib/send_text.rb
+++ lib/send_text.rb
@@ -5,7 +5,7 @@ class TextMessage
 
     url = URI.parse('https://www.google.com/accounts/ClientLogin')
     login_req = Net::HTTP::Post.new(url.path)
-    login_req.form_data = {'accountType' => 'GOOGLE', 'Email' => 'twenty4play@gmail.com', 'Passwd' => '*******', 'service' => 'grandcentral', 'source' => 'tiare.herokuapp.com'}
+    login_req.form_data = {'accountType' => 'GOOGLE', 'Email' => 'twenty4play@gmail.com', 'Passwd' => 'judamang', 'service' => 'grandcentral', 'source' => 'tiare.herokuapp.com'}
     login_con = Net::HTTP.new(url.host, url.port)
     login_con.use_ssl = true
     login_con.start { |http| login_response = http.request(login_req) }
@@ -13,7 +13,7 @@ class TextMessage
     url = URI.parse('https://www.google.com/voice/sms/send/')
     req = Net::HTTP::Post.new(url.path, {'Content-type' => 'application/x-www-form-urlencoded', 'Authorization' => 'GoogleLogin auth='+login_response.body.match("Auth\=(.*)")[0].gsub("Auth=", "")})
 
-    req.form_data = {'id' => '', 'phoneNumber' => '*********', 'text' => message, '_rnr_se' => 'BJQn6mVVF+L/PIvaPaaBWpGXTBY='}
+    req.form_data = {'id' => '', 'phoneNumber' => '3036671970', 'text' => message, '_rnr_se' => 'BJQn6mVVF+L/PIvaPaaBWpGXTBY='}
     con = Net::HTTP.new(url.host, url.port)
     con.use_ssl = true
     con.start { |http| response = http.request(req) }
