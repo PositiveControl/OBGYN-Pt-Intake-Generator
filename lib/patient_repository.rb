f613bdde7ba9603121db56209c575d2d93891e2b
Mark Evans <ga1j1n@mac.com>
2014-04-09 00:39:06 -0600

    Added production Google Voice and Heroku Postgres account credentials

--- lib/patient_repository.rb
+++ lib/patient_repository.rb
@@ -2,7 +2,7 @@ require 'patient'
 require 'sequel'
 require 'pg'
 
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://kjlbwsbjncokzo:mVnz_sQJ2e9Z4LbxEvusls6I3q@ec2-54-83-196-217.compute-1.amazonaws.com:5432/da9j68q9s6tv5u')
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://tsbtefmaohugpv:HDNKz4GILKroDp8N8KIV18sml0@ec2-54-83-196-217.compute-1.amazonaws.com:5432/dfau6tcjh64s8v')
 
 class PatientRepository
 
--- lib/send_text.rb
+++ lib/send_text.rb
@@ -5,7 +5,7 @@ class TextMessage
 
     url = URI.parse('https://www.google.com/accounts/ClientLogin')
     login_req = Net::HTTP::Post.new(url.path)
-    login_req.form_data = {'accountType' => 'GOOGLE', 'Email' => 'twenty4play@gmail.com', 'Passwd' => 'password', 'service' => 'grandcentral', 'source' => 'tiare.herokuapp.com'}
+    login_req.form_data = {'accountType' => 'GOOGLE', 'Email' => 'pt.intake.generator@gmail.com', 'Passwd' => 'super easy password', 'service' => 'grandcentral', 'source' => 'tiare.herokuapp.com'}
     login_con = Net::HTTP.new(url.host, url.port)
     login_con.use_ssl = true
     login_con.start { |http| login_response = http.request(login_req) }
@@ -13,7 +13,7 @@ class TextMessage
     url = URI.parse('https://www.google.com/voice/sms/send/')
     req = Net::HTTP::Post.new(url.path, {'Content-type' => 'application/x-www-form-urlencoded', 'Authorization' => 'GoogleLogin auth='+login_response.body.match("Auth\=(.*)")[0].gsub("Auth=", "")})
 
-    req.form_data = {'id' => '', 'phoneNumber' => '3036671970', 'text' => message, '_rnr_se' => 'BJQn6mVVF+L/PIvaPaaBWpGXTBY='}
+    req.form_data = {'id' => '', 'phoneNumber' => '7202893911', 'text' => message, '_rnr_se' => '45rXiQlP071fDsvDOR1ioKhH94s='}
     con = Net::HTTP.new(url.host, url.port)
     con.use_ssl = true
     con.start { |http| response = http.request(req) }
